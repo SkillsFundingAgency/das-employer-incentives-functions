4f0df4cdf3745fb0249dd227bd2d3a6081a74faf
Dave Read <david.read@digital.education.gov.uk>
2020-06-26 13:26:07 +0100

    EI-72 add placeholder API client

---
+++ src/SFA.DAS.EmployerIncentives.Infrastructure/ApiClient/CalculatePaymentApiClient.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerIncentives.Infrastructure.ApiClient
+{
+    public class CalculatePaymentApiClient : ApiClientBase<CalculatePaymentApiClient>, ICalculatePaymentApiClient
+    {
+        public CalculatePaymentApiClient(HttpClient httpClient, ILogger<CalculatePaymentApiClient> logger)
+        : base(httpClient, logger)
+        {
+        }
+
+        public async Task CalculateFirstPayment(Guid claimId)
+        {
+            // TODO: integrate with outer API
+
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives.Infrastructure/ApiClient/ICalculatePaymentApiClient.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerIncentives.Infrastructure.ApiClient
+{
+    public interface ICalculatePaymentApiClient 
+    {
+        Task CalculateFirstPayment(Guid claimId);
+    }
+}
