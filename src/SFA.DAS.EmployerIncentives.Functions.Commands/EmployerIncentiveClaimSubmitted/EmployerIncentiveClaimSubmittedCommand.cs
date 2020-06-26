099f2e78b6156b79fe5e1f5a80eb84b054db9f0d
Dave Read <david.read@digital.education.gov.uk>
2020-06-26 13:25:44 +0100

    EI-72 create commands project

---
+++ src/SFA.DAS.EmployerIncentives.Functions.Commands/EmployerIncentiveClaimSubmitted/EmployerIncentiveClaimSubmittedCommand.cs
@@ -0,0 +1,15 @@
+﻿using SFA.DAS.EmployerIncentives.Infrastructure.Commands;
+using System;
+
+namespace SFA.DAS.EmployerIncentives.Functions.Commands.EmployerIncentiveClaimSubmitted
+{
+    public class EmployerIncentiveClaimSubmittedCommand : ICommand
+    {
+        public Guid ClaimId { get; private set; }
+
+        public EmployerIncentiveClaimSubmittedCommand(Guid claimId)
+        {
+            ClaimId = claimId;
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives.Functions.Commands/EmployerIncentiveClaimSubmitted/EmployerIncentiveClaimSubmittedCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using SFA.DAS.EmployerIncentives.Infrastructure.Commands;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerIncentives.Functions.Commands.EmployerIncentiveClaimSubmitted
+{
+    public class EmployerIncentiveClaimSubmittedCommandValidator : IValidator<EmployerIncentiveClaimSubmittedCommand>
+    {
+        public Task<ValidationResult> Validate(EmployerIncentiveClaimSubmittedCommand item)
+        {
+            var result = new ValidationResult();
+
+            if (item.ClaimId == default)
+            {
+                result.AddError("ClaimId", "Is not set");
+            }
+
+            return Task.FromResult(result);
+        }
+    }
+}
