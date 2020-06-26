fae531f4398403475ddf8f47c467c8da794df594
Dave Read <david.read@digital.education.gov.uk>
2020-06-26 13:25:16 +0100

    EI-72 add infrastructure project

---
+++ src/SFA.DAS.EmployerIncentives.Infrastructure/QueueNames.cs
@@ -0,0 +1,8 @@
+﻿namespace SFA.DAS.EmployerIncentives.Infrastructure
+{
+    public static class QueueNames
+    {
+        public const string LegalEntityAdded = "SFA.DAS.EmployerIncentives.Jobs.LegalEntityAdded";
+        public const string EmployerIncentiveClaimSubmitted = "SFA.DAS.EmployerIncentives.Jobs.EmployerIncentiveClaimSubmitted";
+    }
+}
