ba3ba8894023f6db20d313fef2ecc4a9c382a6d2
Dave Read <david.read@digital.education.gov.uk>
2020-06-29 17:28:15 +0100

    EI-72 move message to shared Nuget package

--- src/SFA.DAS.EmployerIncentives.Functions/HandleEmployerIncentiveClaimSubmittedEvent.cs
+++ src/SFA.DAS.EmployerIncentives.Functions/HandleEmployerIncentiveClaimSubmittedEvent.cs
@@ -1,16 +1,10 @@
-using System;
-using System.IO;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 using SFA.DAS.EmployerIncentives.Infrastructure.Commands;
 using SFA.DAS.EmployerIncentives.Functions.Commands.EmployerIncentiveClaimSubmitted;
 using SFA.DAS.NServiceBus.AzureFunction.Attributes;
 using SFA.DAS.EmployerIncentives.Infrastructure;
+using SFA.DAS.EmployerIncentives.Messages;
 
 namespace SFA.DAS.EmployerIncentives.Functions
 {
