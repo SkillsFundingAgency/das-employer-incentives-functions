72f18354ce02fe5891bb2005aff88289dd71a824
Ruslan <ruslanss@gmail.com>
2020-10-01 16:38:42 +0100

    RefreshVendorRegistrationCaseStatus: update unit tests

---
+++ src/SFA.DAS.EmployerIncentives.Functions.UnitTests/Services/VrfCaseRefresh/WhenRefreshStatuses.cs
@@ -0,0 +1,43 @@
+﻿using Moq;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Functions.LegalEntities;
+using SFA.DAS.EmployerIncentives.Functions.LegalEntities.Services.LegalEntities;
+using System;
+using System.Threading.Tasks;
+
+namespace SFA.DAS.EmployerIncentives.Functions.UnitTests.Services.VrfCaseRefresh
+{
+    public class WhenRefreshStatuses
+    {
+        private IVrfCaseRefreshService _sut;
+        private readonly Mock<IVendorRegistrationFormService> _vrfService = new Mock<IVendorRegistrationFormService>();
+        private readonly Mock<IVrfCaseRefreshRepository> _repository = new Mock<IVrfCaseRefreshRepository>();
+        private readonly Mock<IDateTimeProvider> _dateTimeProvider = new Mock<IDateTimeProvider>();
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new VrfCaseRefreshService(_vrfService.Object,
+                _repository.Object,
+                _dateTimeProvider.Object);
+        }
+
+        [Test]
+        public async Task
+            Then_API_is_invoked_with_LastRunDateTime_as_FromDateTime_and_current_DateTime_as_ToDateTime_and_last_Run_DateTime_is_updated()
+        {
+            // Arrange
+            var lastRunDateTime = DateTime.UtcNow.AddHours(-1);
+            _repository.Setup(x => x.GetLastRunDateTime()).ReturnsAsync(lastRunDateTime);
+            var currentDateTime = DateTime.UtcNow;
+            _dateTimeProvider.Setup(x => x.GetCurrentDateTime()).ReturnsAsync(currentDateTime);
+
+            // Act
+            await _sut.RefreshStatuses();
+
+            // Assert
+            _vrfService.Verify(x => x.Update(lastRunDateTime, currentDateTime), Times.Once());
+            _repository.Verify(x => x.UpdateLastRunDateTime(currentDateTime), Times.Once);
+        }
+    }
+}
