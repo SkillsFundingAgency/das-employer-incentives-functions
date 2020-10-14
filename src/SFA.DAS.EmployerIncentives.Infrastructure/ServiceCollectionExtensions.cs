a41a9ab87b69aac34498fef2362fe7699c45d904
Kevin <kevin@stephek.co.uk>
2020-10-14 16:22:42 +0100

    EI-469-fix-logging

--- src/SFA.DAS.EmployerIncentives.Infrastructure/ServiceCollectionExtensions.cs
+++ src/SFA.DAS.EmployerIncentives.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,8 +1,6 @@
 ﻿using Microsoft.Azure.WebJobs.Host.Config;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NLog.Extensions.Logging;
-using SFA.DAS.EmployerIncentives.Infrastructure.Logging;
 using SFA.DAS.NServiceBus.AzureFunction.Configuration;
 using SFA.DAS.NServiceBus.AzureFunction.Hosting;
 using System;
@@ -11,26 +9,6 @@ namespace SFA.DAS.EmployerIncentives.Infrastructure
 {
     public static class ServiceCollectionExtensions
     {
-        public static IServiceCollection AddNLog(this IServiceCollection serviceCollection)
-        {
-            var nLogConfiguration = new NLogConfiguration();
-
-            serviceCollection.AddLogging((options) =>
-            {
-                options.SetMinimumLevel(LogLevel.Trace);
-                options.AddNLog(new NLogProviderOptions
-                {
-                    CaptureMessageTemplates = true,
-                    CaptureMessageProperties = true
-                });
-                options.AddConsole();
-
-                nLogConfiguration.ConfigureNLog();
-            });
-
-            return serviceCollection;
-        }
-
         public static IServiceCollection AddNServiceBus(
             this IServiceCollection serviceCollection,
             ILogger logger,
