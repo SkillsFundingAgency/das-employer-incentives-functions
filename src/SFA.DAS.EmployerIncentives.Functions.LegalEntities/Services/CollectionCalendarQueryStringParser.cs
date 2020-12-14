d5e7742a5eb4e8d41c0e642db23ee204ff0c07ab
Dave Read <david.read@digital.education.gov.uk>
2020-12-14 17:52:14 +0000

    EI-610 provide validation error detail for invalid query string

---
+++ src/SFA.DAS.EmployerIncentives.Functions.LegalEntities/Services/CollectionCalendarQueryStringParser.cs
@@ -0,0 +1,66 @@
+﻿using SFA.DAS.EmployerIncentives.Functions.LegalEntities.Services.LegalEntities.Types;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SFA.DAS.EmployerIncentives.Functions.LegalEntities.Services
+{
+    public static class CollectionCalendarQueryStringParser
+    {
+        public static CollectionCalendarUpdateRequest ParseQueryString(IDictionary<string, string> queryParameters, out string validationMessage)
+        {
+            var validationErrors = new List<string>();
+            short calendarYear = 0;
+            byte periodNumber = 0;
+            bool active = false;
+
+            if (!queryParameters.ContainsKey("CalendarYear"))
+            {
+                validationErrors.Add("CalendarYear not set");
+            }
+            if (!queryParameters.ContainsKey("PeriodNumber"))
+            {
+                validationErrors.Add("PeriodNumber not set");
+            }
+            if (!queryParameters.ContainsKey("Active"))
+            {
+                validationErrors.Add("Active not set");
+            }
+
+            if (queryParameters.ContainsKey("CalendarYear"))
+            {
+                var validCalendarYear = short.TryParse(queryParameters["CalendarYear"], out calendarYear);
+                if (!validCalendarYear)
+                {
+                    validationErrors.Add("Invalid value for CalendarYear");
+                }
+            }
+
+            if (queryParameters.ContainsKey("PeriodNumber"))
+            {
+                var validPeriodNumber = byte.TryParse(queryParameters["PeriodNumber"], out periodNumber);
+                if (!validPeriodNumber)
+                {
+                    validationErrors.Add("Invalid value for PeriodNumber");
+                }
+            }
+
+            if (queryParameters.ContainsKey("Active"))
+            {
+                var validActive = bool.TryParse(queryParameters["Active"], out active);
+                if (!validActive)
+                {
+                    validationErrors.Add("Invalid value for Active");
+                }
+            }
+
+            if (validationErrors.Any())
+            {
+                validationMessage = string.Join(". ", validationErrors);
+                return null;
+            }
+
+            validationMessage = string.Empty;
+            return new CollectionCalendarUpdateRequest(calendarYear, periodNumber, active);
+        }
+    }
+}
---
+++ src/SFA.DAS.EmployerIncentives.Functions.UnitTests/Services/CollectionCalendar/WhenParsingCollectionCalendarQueryString.cs
@@ -0,0 +1,105 @@
+﻿
+using FluentAssertions;
+using NUnit.Framework;
+using SFA.DAS.EmployerIncentives.Functions.LegalEntities.Services;
+using System.Collections.Generic;
+
+namespace SFA.DAS.EmployerIncentives.Functions.UnitTests.Services.CollectionCalendar
+{
+    [TestFixture]
+    public class WhenParsingCollectionCalendarQueryString
+    {
+        private Dictionary<string, string> _queryStringDictionary;
+        private string _validationMessage;
+
+        [SetUp]
+        public void Arrange()
+        {
+            _queryStringDictionary = new Dictionary<string, string>();
+            _queryStringDictionary.Add("CalendarYear", "2020");
+            _queryStringDictionary.Add("PeriodNumber", "1");
+            _queryStringDictionary.Add("Active", "true");
+        }
+
+        [Test]
+        public void Then_no_errors_are_triggered_for_a_valid_query_string()
+        {
+            var requestObject = CollectionCalendarQueryStringParser.ParseQueryString(_queryStringDictionary, out _validationMessage);
+
+            requestObject.Should().NotBeNull();
+            _validationMessage.Length.Should().Be(0);
+        }
+
+        [Test]
+        public void Then_an_error_is_returned_if_calendar_year_missing()
+        {
+            _queryStringDictionary.Remove("CalendarYear");
+            var requestObject = CollectionCalendarQueryStringParser.ParseQueryString(_queryStringDictionary, out _validationMessage);
+
+            requestObject.Should().BeNull();
+            _validationMessage.Should().Contain("CalendarYear not set");
+        }
+
+        [TestCase("202A")]
+        [TestCase("*&*^%^")]
+        [TestCase("ABCD")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void Then_an_error_is_returned_if_calendar_year_invalid(string calendarYearValue)
+        {
+            _queryStringDictionary["CalendarYear"] = calendarYearValue;
+            var requestObject = CollectionCalendarQueryStringParser.ParseQueryString(_queryStringDictionary, out _validationMessage);
+
+            requestObject.Should().BeNull();
+            _validationMessage.Should().Contain("Invalid value for CalendarYear");
+        }
+
+        [Test]
+        public void Then_an_error_is_returned_if_period_missing()
+        {
+            _queryStringDictionary.Remove("PeriodNumber");
+            var requestObject = CollectionCalendarQueryStringParser.ParseQueryString(_queryStringDictionary, out _validationMessage);
+
+            requestObject.Should().BeNull();
+            _validationMessage.Should().Contain("PeriodNumber not set");
+        }
+
+        [TestCase("2A")]
+        [TestCase("*&*^%^")]
+        [TestCase("ABCD")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void Then_an_error_is_returned_if_period_invalid(string periodValue)
+        {
+            _queryStringDictionary["PeriodNumber"] = periodValue;
+            var requestObject = CollectionCalendarQueryStringParser.ParseQueryString(_queryStringDictionary, out _validationMessage);
+
+            requestObject.Should().BeNull();
+            _validationMessage.Should().Contain("Invalid value for PeriodNumber");
+        }
+
+        [Test]
+        public void Then_an_error_is_returned_if_active_missing()
+        {
+            _queryStringDictionary.Remove("Active");
+            var requestObject = CollectionCalendarQueryStringParser.ParseQueryString(_queryStringDictionary, out _validationMessage);
+
+            requestObject.Should().BeNull();
+            _validationMessage.Should().Contain("Active not set");
+        }
+
+        [TestCase("A")]
+        [TestCase("*&*^%^")]
+        [TestCase("1")]
+        [TestCase("")]
+        [TestCase(null)]
+        public void Then_an_error_is_returned_if_active_invalid(string activeValue)
+        {
+            _queryStringDictionary["Active"] = activeValue;
+            var requestObject = CollectionCalendarQueryStringParser.ParseQueryString(_queryStringDictionary, out _validationMessage);
+
+            requestObject.Should().BeNull();
+            _validationMessage.Should().Contain("Invalid value for Active");
+        }
+    }
+}
