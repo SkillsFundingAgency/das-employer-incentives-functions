c3239878e84b65105a7c38953af8b800aea4115f
David-B-Read <david.read@digital.education.gov.uk>
2022-06-17 13:14:56 +0100

    EI-1851 fix Sonar warning

--- src/SFA.DAS.EmployerIncentives.Functions.LegalEntities/Services/RevertPayments/RevertPaymentsServiceException.cs
+++ src/SFA.DAS.EmployerIncentives.Functions.LegalEntities/Services/RevertPayments/RevertPaymentsServiceException.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Net;
+using System.Runtime.Serialization;
 
 namespace SFA.DAS.EmployerIncentives.Functions.LegalEntities.Services.RevertPayments
 {
+    [Serializable]
     public class RevertPaymentsServiceException : Exception
     {
         public HttpStatusCode HttpStatusCode { get; }
@@ -12,6 +14,11 @@ namespace SFA.DAS.EmployerIncentives.Functions.LegalEntities.Services.RevertPaym
         {
             HttpStatusCode = httpStatusCode;
             Content = content;
+        }
+
+        protected RevertPaymentsServiceException(SerializationInfo serializationInfo, StreamingContext streamingContext) 
+            : base(serializationInfo, streamingContext)
+        {
         }
     }
 }
