5dbe6bc15dcbbc21a733173857e9532724b24a0e
David-B-Read <david.read@digital.education.gov.uk>
2023-05-25 21:39:37 +0100

    EI-2017 reinstate missing acceptance test

---
+++ src/Support/SFA.DAS.EmployerIncentives.Functions.Support.AcceptanceTests/Features/LegalEntities.feature
@@ -0,0 +1,11 @@
+﻿@messageBus
+@employerIncentivesApi
+Feature: LegalEntities
+	When a legal entity state changes
+	Then is is forwarded to the EmployerIncentives system
+
+Scenario: A job to load all legal entities is requested
+	When a request to refresh legal entities is received	
+	Then the request is forwarded to the Employer Incentives system
+
+
---
+++ src/Support/SFA.DAS.EmployerIncentives.Functions.Support.AcceptanceTests/Steps/LegalEntitySteps.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json;
+using NServiceBus.Transport;
+using SFA.DAS.EmployerIncentives.Functions.Support.AcceptanceTests.Extensions;
+using SFA.DAS.EmployerIncentives.Functions.Support.Services.Jobs.Types;
+using SFA.DAS.EmployerIncentives.Messages.Events;
+using TechTalk.SpecFlow;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+
+namespace SFA.DAS.EmployerIncentives.Functions.Support.AcceptanceTests.Steps
+{
+    [Binding]
+    [Scope(Feature = "LegalEntities")]
+    public class LegalEntitySteps : StepsBase    
+    {
+        private readonly TestContext _testContext;
+        
+        public LegalEntitySteps(TestContext testContext) : base(testContext)
+        {
+            _testContext = testContext;
+        }
+
+        [When(@"a request to refresh legal entities is received")]
+        public async Task WhenRefreshLegalEntitiesRequestIsReceived()
+        {
+            var jobRequest = _testContext.TestData.GetOrCreate(onCreate: () =>
+            {
+                return new JobRequest
+                {
+                    Type = JobType.RefreshLegalEntities,
+                    Data = new Dictionary<string, string>
+                    {
+                        { "PageNumber", "1" },
+                        { "PageSize", "200" }
+                    }
+                };
+            });
+
+            _testContext.EmployerIncentivesApi.MockServer
+               .Given(
+                       Request
+                       .Create()
+                       .WithPath(x => x.Contains("legalentities/refresh"))
+                       .UsingPut()
+                       )
+                   .RespondWith(
+               Response.Create()
+                   .WithStatusCode(HttpStatusCode.OK)
+                   .WithHeader("Content-Type", "application/json"));
+
+            await _testContext.LegalEntitiesFunctions.HttpTriggerRefreshLegalEntities.RunHttp(null);
+        }
+
+        [Then(@"a request is made to the Employer Incentives system")]
+        public void TheARequestIsMadeToTheApi()
+        {
+            var jobRequest = _testContext.TestData.GetOrCreate<JobRequest>();
+
+            var requests = _testContext
+                .EmployerIncentivesApi
+                .MockServer
+                .FindLogEntries(
+                    Request
+                        .Create()
+                        .WithPath($"/api/jobs")
+                        .WithBody(JsonConvert.SerializeObject((object)jobRequest))
+                );
+
+            requests.AsEnumerable().Count().Should().Be(1);
+        }
+
+        [Then(@"the request is forwarded to the Employer Incentives system")]
+        public void ThenTheRefreshRequestIsForwardedToTheApi()
+        {
+            var jobRequest = _testContext.TestData.GetOrCreate<JobRequest>();
+
+            var requests = _testContext
+                       .EmployerIncentivesApi
+                       .MockServer
+                       .FindLogEntries(
+                           Request
+                           .Create()
+                           .WithPath(x => x.Contains("legalentities/refresh")));
+
+            requests.AsEnumerable().Count().Should().Be(1);
+        }
+    }    
+}
